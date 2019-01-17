098cab7691f0442461a7f093ee60712171fe102b
CM Student <cmstudent@UHWO-ACM-C-01.local>
2019-01-17 10:38:42 -1000

    Almost Completed Swiftplayground

--- SwiftPlaygroundsOne.playground/Contents.swift
+++ SwiftPlaygroundsOne.playground/Contents.swift
@@ -3,3 +3,14 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Swift Playground #1
+//Below we declare and set a variable to data type String
+var myFirstVariable: String
+//Now we assign varibale to a string
+myFirstVariable = "I've created my first varibale"
+//Delcare and assign all in one line
+var mySecondVarible = "and look I added two strings together"
+//Let's add to strings together
+var myThirdVariable = myFirstVariable + mySecondVarible
+print(myThirdVariable)
